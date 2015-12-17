76b0026c28624299c36f29f118920cfaf8513997
Slavek Kabrda <bkabrda@redhat.com>
2015-12-17 18:53:19 +0100

    We now have README.rst instead of README.md

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='flexmock',
       py_modules=['flexmock'],
       description='flexmock is a testing library for Python that makes it easy to create mocks,'
                   'stubs and fakes.',
-      long_description=codecs.open('README.md', encoding='utf8').read(),
+      long_description=codecs.open('README.rst', encoding='utf8').read(),
       keywords='flexmock mock stub test unittest pytest nose',
       classifiers=[
           'License :: OSI Approved :: BSD License',
