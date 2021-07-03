303834cac0c96f48c9e34cf1e334a4328476bdd1
Olli Paakkunainen <olli@paakkunainen.fi>
2021-07-03 21:34:44 +0900

    Organize code

---
+++ flexmock/exceptions.py
@@ -0,0 +1,33 @@
+"""Flecmock exceptions."""
+
+
+class FlexmockError(Exception):
+    """FlexmockError"""
+
+
+class MockBuiltinError(Exception):
+    """MockBuiltinError"""
+
+
+class MethodSignatureError(FlexmockError):
+    """MethodSignatureError"""
+
+
+class ExceptionClassError(FlexmockError):
+    """ExceptionClassError"""
+
+
+class ExceptionMessageError(FlexmockError):
+    """ExceptionMessageError"""
+
+
+class StateError(FlexmockError):
+    """StateError"""
+
+
+class MethodCallError(FlexmockError):
+    """MethodCallError"""
+
+
+class CallOrderError(FlexmockError):
+    """CallOrderError"""
