8c38c784ef274252e748736590f5da4d75d050d5
Olli Paakkunainen <olli@paakkunainen.fi>
2021-07-15 10:28:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
-import codecs
+"""Flexmock setup.py."""
+from setuptools import setup
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
 
-setup(name='flexmock',
-      version='0.10.5',
-      author='Slavek Kabrda, Herman Sheremetyev',
-      author_email='slavek@redhat.com',
-      url='http://flexmock.readthedocs.org',
-      license='BSD License',
-      py_modules=['flexmock'],
-      description='flexmock is a testing library for Python that makes it easy to create mocks,'
-                  'stubs and fakes.',
-      long_description=codecs.open('README.rst', encoding='utf8').read(),
-      keywords='flexmock mock stub test unittest pytest nose',
-      classifiers=[
-          'License :: OSI Approved :: BSD License',
-          'Intended Audience :: Developers',
-          'Development Status :: 5 - Production/Stable',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: Implementation :: Jython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Topic :: Software Development :: Testing',
-      ],
-      )
+setup(
+    name="flexmock",
+    version="0.11.0",
+    author="Slavek Kabrda, Herman Sheremetyev",
+    author_email="slavek@redhat.com",
+    url="https://flexmock.readthedocs.io/",
+    license="BSD-2-Clause",
+    description="flexmock is a testing library for Python that makes it easy to create mocks,"
+    "stubs and fakes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="flexmock mock stub test unittest pytest nose",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Testing",
+    ],
+    python_requires=">=3.6.2,<4.0.0",
+    packages=["flexmock"],
+    package_dir={"": "src"},
+    include_package_data=True,
+)
