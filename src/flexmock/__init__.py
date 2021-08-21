a9aac07f521018f227ab9d910054088ce5aa980b
Olli Paakkunainen <olli@paakkunainen.fi>
2021-08-21 18:19:16 +0900

    Make integrations module private

--- src/flexmock/__init__.py
+++ src/flexmock/__init__.py
@@ -1,5 +1,5 @@
 """Flexmock testing library for Python."""
-from flexmock import integrations  # pylint: disable=unused-import
+from flexmock import _integrations  # pylint: disable=unused-import
 from flexmock.api import flexmock
 
 __all__ = ["flexmock"]
