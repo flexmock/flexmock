dc8025e304c80e91da14b9e79f0e2b1fbb490884
Tomasz Kłoczko <kloczek@github.com>
2024-03-16 14:58:39 +0000

    really drop python<=3.7 support
    Filer all code over `pyupgrade --py38-plus`.
    
    Signed-off-by: Tomasz Kłoczko <kloczek@github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as file:
+with open("README.md", encoding="utf-8") as file:
     long_description = file.read()
 
 VERSION = "0.12.0"
